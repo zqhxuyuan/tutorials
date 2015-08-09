8c1bfd44e3726474c16414943f764617370df043
zqhxuyuan <zqhxuyuan@gmail.com>
2015-08-09 23:03:57 +0800

    akka

--- scala/src/main/scala/com/zqh/akka/notes/StudentDelayActor.scala
+++ scala/src/main/scala/com/zqh/akka/notes/StudentDelayActor.scala
@@ -12,9 +12,15 @@ class StudentDelayActor (teacherActorRef:ActorRef) extends Actor with ActorLoggi
 
   def receive = {
     case InitSignal=> {
+      //直接发送,没有任何延迟
       //teacherActorRef!QuoteRequest
+
+      //一次性调度,开始时间延迟5秒
       import context.dispatcher
       context.system.scheduler.scheduleOnce(5 seconds, teacherActorRef, QuoteRequest)
+
+      //定时调度: 没有延迟,每隔5秒发送一次
+      //context.system.scheduler.schedule(0 seconds, 5 seconds, teacherActorRef, QuoteRequest)
     }
 
     case QuoteResponse(quoteString) => {
